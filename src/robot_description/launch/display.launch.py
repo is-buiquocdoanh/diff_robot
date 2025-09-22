10e5dad8907016ef30bbef63e150e2e80bf34173
Jayson_Bui <doanh762003@gmail.com>
2025-09-22 23:02:02 +0700

    simulation gazebo

--- src/robot_description/launch/display.launch.py
+++ src/robot_description/launch/display.launch.py
@@ -38,5 +38,5 @@ def generate_launch_description():
     return LaunchDescription([
         joint_state_publisher_node,
         robot_state_publisher_node,
-        rviz_node
+        # rviz_node
     ])
--- src/robot_description/setup.py
+++ src/robot_description/setup.py
@@ -14,6 +14,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro') + glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
